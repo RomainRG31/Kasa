2af046878fd5e8d3e35b321d44a103e00fc04823
RomainRG31 <romain32.roger@gmail.com>
2024-05-13 10:50:01 +0200

    changes craco config

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,10 @@
 // fonction pour mettre le fichier src comme fichier de ref avec le @ dans les appels d'imports
-const path = require('path')
+const path = require("path");
+
 module.exports = {
-    webpack: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        }
-    }
-}
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
+};
