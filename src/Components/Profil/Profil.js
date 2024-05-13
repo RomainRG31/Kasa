f06e153b8358bc900d6be23ee9a5d401cf95fadd
RomainRG31 <romain32.roger@gmail.com>
2024-05-13 10:33:18 +0200

    some changes

--- src/Components/Profil/Profil.js
+++ src/Components/Profil/Profil.js
@@ -1,15 +1,14 @@
 import "@/Components/Profil/Profil.css";
 
 const Profil = ({ host }) => {
+  if (!host) return;
 
-    if (!host) return;
-
-    return (
-        <div className="profil">
-            <p className="profil_name">{host.name}</p>
-            <img className="round_img" src={host.picture} alt="Photo  de profil" />
-        </div>
-    )
-}
+  return (
+    <div className="profil">
+      <p className="profil_name">{host.name}</p>
+      <img className="round_img" src={host.picture} alt="profil" />
+    </div>
+  );
+};
 
 export default Profil;
--- src/Pages/Public/Fiche-logement/Logement.js
+++ src/Pages/Public/Fiche-logement/Logement.js
@@ -1,8 +1,8 @@
 // import des modules necessaires
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import LogementService from "@/_Services/Logement.service.js"
+import LogementService from "@/_Services/Logement.service.js";
 
 import "@/Pages/Public/Fiche-logement/Logement.css";
 
@@ -13,92 +13,83 @@ import Tag from "@/Components/Tag/Tag";
 import Stars from "@/Components/Stars/Stars";
 // fonction de la page Logement
 const Logement = () => {
+  const [logement, setLogement] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
+  let { id } = useParams();
 
-    const [logement, setLogement] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getInfo();
 
-    let { id } = useParams();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  const getInfo = async () => {
+    await LogementService.GetOneLogement(id)
+      .then((data) => {
+        setLogement(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    useEffect(() => {
-
-        getInfo();
-
-    }, [])
-
-    const getInfo = async () => {
-        await LogementService.GetOneLogement(id)
-            .then((data) => {
-                setLogement(data);
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+  const displayTags = (tags) => {
+    if (tags) {
+      return (
+        <div className="tags_container">
+          {tags.map((tag, index) => {
+            return <Tag tag={tag} key={index} />;
+          })}
+        </div>
+      );
     }
+  };
 
-    const displayTags = (tags) => {
-        if (tags) {
-            return (
-                <div className="tags_container">
-                    {tags.map((tag, index) => {
-
-                        return (
-                            <Tag tag={tag} key={index} />
-                        )
-                    })}
-                </div>
-            )
-        }
-
+  const displayEquipments = (equipments) => {
+    if (equipments) {
+      return (
+        <ul>
+          {equipments.map((equipement, index) => {
+            return <li key={index}>{equipement}</li>;
+          })}
+        </ul>
+      );
     }
+  };
 
-    const displayEquipments = (equipments) => {
-        if (equipments) {
-            return (
-                <ul>
-                    {equipments.map((equipement, index) => {
-
-                        return (
-                            <li key={index}>
-                                {equipement}
-                            </li>
-                        )
-                    })}
-                </ul>
-            )
-        }
-    }
-
-
-    if (isLoading) return (<h3>Chargement...</h3>)
-    return (
-
-        <section className="logement">
-            <Gallery pictures={logement.pictures} />
-            <div className="details_container">
-                <div className="details_title_container">
-                    <article className="title_container">
-                        <h2 className="about_title">{logement.title}</h2>
-                        <h3 className="about_subtitle">{logement.location}</h3>
-                    </article>
-                    {displayTags(logement.tags)}
-                </div>
-                <div className="details_profil_container">
-                    <Profil host={logement.host} />
-                    <Stars rating={logement.rating} />
-                </div>
-            </div>
-            <div className="accordeon_container">
-                <DropDown title={"Description"} description={logement.description}></DropDown>
-                <DropDown title={"Équipements"} description={displayEquipments(logement.equipments)}></DropDown>
-            </div>
-        </section>
-
-    );
+  if (isLoading) return <h3>Chargement...</h3>;
+  return (
+    <section className="logement">
+      <Gallery pictures={logement.pictures} />
+      <div className="details_container">
+        <div className="details_title_container">
+          <article className="title_container">
+            <h2 className="about_title">{logement.title}</h2>
+            <h3 className="about_subtitle">{logement.location}</h3>
+          </article>
+          {displayTags(logement.tags)}
+        </div>
+        <div className="details_profil_container">
+          <Profil host={logement.host} />
+          <Stars rating={logement.rating} />
+        </div>
+      </div>
+      <div className="accordeon_container">
+        <DropDown
+          title={"Description"}
+          description={logement.description}
+        ></DropDown>
+        <DropDown
+          title={"Équipements"}
+          description={displayEquipments(logement.equipments)}
+        ></DropDown>
+      </div>
+    </section>
+  );
 };
 
-// export de la page Logement pour l'appeler dans le router 
+// export de la page Logement pour l'appeler dans le router
 
 export default Logement;
